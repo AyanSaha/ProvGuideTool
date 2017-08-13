e53a99229438165268600b922e83bcba0f37bdc9
Ayan Saha <ayan.saha91@gmail.com>
2017-08-13 13:46:07 +0530

    parsing file structure changes

--- app/models/prov_guide_temp.rb
+++ app/models/prov_guide_temp.rb
@@ -1,35 +1,35 @@
 require 'roo'
 class ProvGuideTemp < ApplicationRecord
 	self.primary_keys= :dialplan,:clli,:npa,:rate_center,:locality,:county
-
+  validates_uniqueness_of :prov, :scope => [:dialplan,:clli,:npa,:rate_center,:locality,:county]
 	begin
 	def self.import(file)
   		message="Products imported!!"
   		spreadsheet = open_spreadsheet(file)
    
-  		header = spreadsheet.row(1)
-  
-  		if header.count !=25
-     		message="Columns must be 25!!Wrong format sheet uploaded!!"
+  		header = spreadsheet.row(6)
+      
+  		if header.count !=13
+     		message="Columns must be 13!!Wrong format sheet uploaded!!"
     		return false,message
   		end
-  unless spreadsheet.row(2).nil?
-  (2..spreadsheet.last_row).each do |i|
+  unless spreadsheet.row(7).nil?
+  (7..spreadsheet.last_row).each do |i|
     row = Hash[[header, spreadsheet.row(i)].transpose]
+    binding.pry
+    #provguidetemp = ProvGuideTemp.find_by_dialplan_and_clli_and_rate_center_and_npa_and_locality_and_county(row["dialplan"],row["clli"],row["rate_center"],row["npa"],row["locality"],row["county"])
     
-    provguidetemp = ProvGuideTemp.find_by_dialplan_and_clli_and_rate_center_and_npa_and_locality_and_county(row["dialplan"],row["clli"],row["rate_center"],row["npa"],row["locality"],row["county"])
-    
-    if provguidetemp.nil?
-        provguidetemp=ProvGuideTemp.new
-       provguidetemp.attributes = row.to_hash
-       provguidetemp.created_by='asaha005c'
-      provguidetemp.last_update_by='asaha005c'
-      provguidetemp.save!
+    #if provguidetemp.nil?
+       #provguidetemp=ProvGuideTemp.new
+       #provguidetemp.attributes = row.to_hash
+       #provguidetemp.created_by='asaha005c'
+       #provguidetemp.last_update_by='asaha005c'
+       #provguidetemp.save!
       return true,message
-    else
-      message="Composite constraints on first 6 columns violated"
-      return false,message
-   end
+    #else
+      #message="Composite constraints on first 6 columns violated"
+      #return false,message
+   #end
 
    
    end
@@ -46,6 +46,7 @@ class ProvGuideTemp < ApplicationRecord
     else raise "Unknown file type: #{file.original_filename}"
     end
   end
-rescue
+rescue=>e
+
 end
 end
